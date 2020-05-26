859ffebd73ea18958e29b7d3510ba03b087167cb
ianhi <ianhuntisaak@gmail.com>
2020-05-25 22:34:03 -0400

    check that output is correct

--- speed-comparison/compare.py
+++ speed-comparison/compare.py
@@ -1,4 +1,4 @@
-from numpy import random, float32
+from numpy import random, float32, mean
 import time
 __all__ = [
     'gen_data',
@@ -13,18 +13,24 @@ def gen_data(n=60000):
     return X, y, bw
 
 
-def time_fxn(f, X, y, bw, verbose=True):
+def time_fxn(f, X, y, bw, verbose=True, correct_answer=None):
     start = time.perf_counter()
-    f(X, y, bw)
+    result = f(X, y, bw)
     end = time.perf_counter()
     if verbose:
-        print(f"{f.__name__}:\t{end-start}")
-    return end-start
+        printout = f"{f.__name__}:\t{end-start}"
+        if correct_answer is not None:
+            acc = mean(100-100*(result - correct_answer)/correct_answer)
+            printout += f'\t{acc}%'
+        print(printout)
+    return end-start, result
 
 
 if __name__ == '__main__':
     import methods
-    data = gen_data()
-    for f_name in methods.__all__:
+    data = gen_data(1000)
+    times = {}
+    times['numpy_for_loop'], correct_answer = time_fxn(methods.numpy_for_loop,*data)
+    for f_name in methods.__all__[1:]:
         fxn = getattr(methods, f_name)
-        time_fxn(fxn, *data)
+        times[f_name], _ = time_fxn(fxn, *data, correct_answer=correct_answer)
